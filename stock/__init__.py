158450968c3e74a6282c2a2b1372537fe01fb5c8
QuixoteHY <1290482442@qq.com>
2019-08-18 23:22:52 +0800

    代码整洁

--- stock/__init__.py
+++ stock/__init__.py
@@ -4,16 +4,3 @@
 # @Github   : https://github.com/QuixoteHY
 # @Email    : 1290482442@qq.com
 # @Describe :
-
-
-def format_to_mongodb(fina_indicators, ts_code):
-    ts_code = ts_code.replace('.', '_')
-    list_docs = list()
-    for indicator_name, v in fina_indicators.items():
-        for time, value in v.items():
-            list_docs.append({'ts_code': ts_code,
-                              'indicator_name': indicator_name,
-                              'indicator_code': fina_indicators_code[indicator_name],
-                              'time': time,
-                              'indicator_value': value})
-    return list_docs
--- transfer/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-# @Time     : 2019-07-29 11:04
-# @Author   : 胡远
-# @Github   : https://github.com/QuixoteHY
-# @Email    : 1290482442@qq.com
-# @Describe :
